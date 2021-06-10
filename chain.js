35ea520e1a406a6422edeb08c12f987dffdc1ad8
SarthakBabar123 <78528443+SarthakBabar123@users.noreply.github.com>
2021-06-10 20:16:56 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,33 @@
+class Chain{
+    constructor(bodyA, point){
+        var options = {
+            bodyA: bodyA,
+            pointB: point,
+            stiffness: 0.01,
+            length: 30
+        }
+        this.chain = Constraint.create(options);
+
+        World.add(world, this.chain);
+        console.log(this.chain);
+    }
+    fly(){
+        this.chain.bodyA = null;
+       
+    }
+    
+    attach(){
+         this.chain.bodyA.attach(point)
+    }
+    
+    display(){
+        if(this.chain.bodyA){
+        var pointA = this.chain.bodyA.position;
+        var pointB = this.chain.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+
+    
+}
